a6a9e4ac95ffe2d07ee93940261690b65c13b72e
Tikpeme <tete.ikpeme@gmail.com>
2023-05-30 19:44:54 -0400

    Implemented email.js and form functionality

--- client/src/components/ContactMe/ContactMe.jsx
+++ client/src/components/ContactMe/ContactMe.jsx
@@ -1,75 +1,104 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./ContactMe.scss";
 import { Form } from "react-router-dom";
+import emailjs from "@emailjs/browser";
 
 function ContactMe() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  const handleSubmit = (e) => {
+  const [formSubmitted, setformSubmitted] = useState(false);
+  const form = useRef();
+
+  const sendEmail = (e) => {
     e.preventDefault();
 
-    // Perform form submission logic here, such as sending the data to a server
+    // Perform form submission logic here,
+    emailjs
+      .sendForm(
+        "service_tos395e",
+        "template_q9ey4fd",
+        form.current,
+        "Gc-HCSO_NvM3CK3de"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
 
     // Reset the form fields
+
     setName("");
     setEmail("");
     setMessage("");
+    setformSubmitted(true);
   };
   return (
     <section className="contactMe-section">
-      Contact
       <div className="contactMe-wrapper">
         <div className="contact-grid">
           <h3 className="contact-title">
             Need some beautiful portraits? Let's work together!
           </h3>
           <div className="form-wrapper">
-            <form>
-              <div className="text-field-wrapper">
-                <div className="text-wrap">
-                  <label className="field-label">Name</label>
-                  <input
-                    type="text"
-                    className="text-field"
-                    id="name"
-                    value={name}
-                    placeholder="Enter your full name"
-                    onChange={(e) => setName(e.target.value)}
-                    required
-                  ></input>
+            {!formSubmitted ? (
+              <form ref={form} onSubmit={sendEmail}>
+                <div className="text-field-wrapper">
+                  <div className="text-wrap">
+                    <label className="field-label">Name</label>
+                    <input
+                      type="text"
+                      className="text-field"
+                      id="name"
+                      value={name}
+                      name="user_name"
+                      placeholder="Enter your full name"
+                      onChange={(e) => setName(e.target.value)}
+                      required
+                    ></input>
+                  </div>
+                  <div className="text-field-spacer"></div>
+                  <div className="text-wrap">
+                    <label className="field-label">Email</label>
+                    <input
+                      type="email"
+                      id="email"
+                      value={email}
+                      className="text-field"
+                      name="user_email"
+                      placeholder="Enter your Email"
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    ></input>{" "}
+                  </div>
                 </div>
-                <div className="text-field-spacer"></div>
-                <div className="text-wrap">
-                  <label className="field-label">Email</label>
-                  <input
-                    type="email"
-                    id="email"
-                    value={email}
-                    className="text-field"
-                    placeholder="Enter your Email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                  ></input>{" "}
-                </div>
+                <label className="field-label">Message</label>
+                <textarea
+                  placeholder="How Can I Help You?"
+                  maxLength="5000"
+                  id="message"
+                  value={message}
+                  name="message"
+                  onChange={(e) => setMessage(e.target.value)}
+                  data-name="message"
+                  className="text-field min-height w-input"
+                ></textarea>
+                <input
+                  type="submit"
+                  value="Let's Work Together"
+                  data-wait="Sending Your Message"
+                  className="white-outlined-btn w-button"
+                ></input>
+              </form>
+            ) : (
+              <div className="success-message">
+                Thank you! Your inquiry was recieved!
               </div>
-              <label className="field-label">Message</label>
-              <textarea
-                placeholder="How Can I Help You?"
-                maxLength="5000"
-                id="message"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                data-name="Message"
-                className="text-field min-height w-input"
-              ></textarea>
-              <input
-                type="submit"
-                value="Let's Work Together"
-                data-wait="Sending Your Message"
-                className="white-outlined-btn w-button"
-              ></input>
-            </form>
+            )}
           </div>
         </div>
       </div>
